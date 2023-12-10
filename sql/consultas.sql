d0f89c05c22b876777e598e9bda109388e254436
amandajoioso <amanda.joioso@usp.br>
2023-12-10 02:01:44 -0300

    Atualizãção dos dados pra alguns turistas terem mais de uma viagem e consulta 3

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -1,1 +1,6 @@
-teste
+SELECT t.nome AS nome_turista, COUNT(v.dataPartida) AS total_viagens
+FROM turistaEspacial t
+JOIN viagem v ON t.passaporte = v.turista
+GROUP BY t.nome
+HAVING COUNT(v.dataPartida) > 3
+ORDER BY nome_turista;
