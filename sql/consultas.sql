bd52fb36c67dabff7964dabb9d9f30fc1ef48670
darlam-alves <darlamalvessilva@gmail.com>
2023-12-10 10:12:25 -0300

    identacao

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -7,11 +7,15 @@ HAVING COUNT(v.dataPartida) > 3
 ORDER BY nome_turista;
 
 --Consulta 1
-SELECT DISTINCT te.nome
+SELECT DISTINCT 
+    te.nome
 FROM turistaEspacial te
-JOIN viagem v ON te.passaporte = v.turista
-JOIN voo vo ON v.pacote = vo.pacote
-JOIN colonia co ON vo.colonia = co.nome
+JOIN viagem v 
+    ON te.passaporte = v.turista
+JOIN voo vo 
+    ON v.pacote = vo.pacote
+JOIN colonia co 
+    ON vo.colonia = co.nome
 WHERE co.nome IN ('ColôniaTerra', 'ColôniaVenus', 'ColôniaLua')
 GROUP BY te.passaporte, te.nome
-HAVING COUNT(DISTINCT co.nome) = 3;
+    HAVING COUNT(DISTINCT co.nome) = 3;
